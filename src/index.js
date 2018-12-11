d2ee26947d8832830d99c7b386f387ce665b504a
haozhaojie <haozhaojie@gotokeep.com>
2018-12-11 15:52:23 +0800

    增加性能数据的打印

--- src/index.js
+++ src/index.js
@@ -2,6 +2,9 @@ import React from "react";
 import ReactDom from "react-dom";
 import { HashRouter } from "react-router-dom";
 import App from "./layout/App.js"
+import performanceMonitor from './util/performance.js'
+
+performanceMonitor.init()
 
 ReactDom.render(
   <HashRouter>
--- src/layout/App.js
+++ src/layout/App.js
@@ -26,6 +26,7 @@ class App extends React.Component {
     }
   }
 
+
   toggleCollapsed() {
     this.setState({
       isCollapsed: !this.state.isCollapsed,
